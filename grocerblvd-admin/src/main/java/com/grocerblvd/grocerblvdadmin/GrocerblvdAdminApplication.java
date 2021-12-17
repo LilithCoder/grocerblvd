831dff04da76cd850c9a136bdc793f270855171f
Weilin Gao <weilinga@usc.edu>
2021-12-17 23:35:22 +0800

    first commit

---
+++ grocerblvd-admin/src/main/java/com/grocerblvd/grocerblvdadmin/GrocerblvdAdminApplication.java
@@ -0,0 +1,13 @@
+package com.grocerblvd.grocerblvdadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerblvdAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerblvdAdminApplication.class, args);
+	}
+
+}
